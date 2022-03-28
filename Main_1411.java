27517800789de270c919ade5e5ecc8b4d935ff04
david <abovenormal5023@gmail.com>
2022-03-29 01:18:40 +0900

    first repo

---
+++ Main_1411.java
@@ -0,0 +1,23 @@
+// 22.03.23
+
+import java.io.*;
+import java.util.*;
+
+public class Main_1411 {
+
+    static int n;
+    static String arr[];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+        arr = new String[n];
+
+        for(int i=0;i<n;i++){
+
+            arr[i] = br.readLine();
+
+        }
+
+    }
+}
